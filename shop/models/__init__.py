cc65e12a7e81d934cbddbfd82388416eb812f13b
Daesu Oh <90598930+ohdaesu2@users.noreply.github.com>
2021-12-27 21:07:09 +0900

    Develop (#1)
    * feat: New Project
    
    * chore: Add gitignore file
    
    * fix: change shoppingbag name and rename mnm

--- shop/models/__init__.py
+++ shop/models/__init__.py
@@ -1,2 +1,2 @@
 from shop.models import brand, category, clothes, size, color, guide
-from shop.models.many_to_many_clothes import clothes_category, clothes_color
+from shop.models.mtm import clothes_category, clothes_color
